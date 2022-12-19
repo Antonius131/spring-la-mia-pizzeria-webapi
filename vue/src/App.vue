fb2dbfd14efa5c39ecc09c619dd7b7fecefbd92b
Antonio Mendetta <Antonio@MBP-di-Antonio.station>
2022-12-19 15:43:03 +0100

    create and import PizzaCompp in vue App

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PizzaComp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import PizzaComp from './components/PizzaComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PizzaComp
   }
 }
 </script>
---
+++ vue/src/components/PizzaComp.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <h1>Hello</h1>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'PizzaComp',
+   props: {
+     
+   }
+ }
+ </script>
+ 
+
+ <style scoped lang="scss">
+
+ </style>
+ 
